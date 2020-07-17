99a102fd4cd45de42c7fe4b40ec9bee9dc11dc3d
hilow2341 <60238749+hilow2341@users.noreply.github.com>
2020-07-17 17:00:27 -0400

    Begin jsx for rendering user profiles

--- user-directory/src/components/DataBody/index.js
+++ user-directory/src/components/DataBody/index.js
@@ -14,3 +14,29 @@ const DataBody = () => {
         const formattedDate =[month, day, year].join("-");
         return formattedDate;
     }
+
+    return (
+        <tbody>
+        {context.developerState.filteredUsers[0] !== undefined && context.developerState.filteredUsers[0].name !== undefined ? (
+          context.developerState.filteredUsers.map(({ login, name, picture, phone, email, dob }) => {
+            return (
+              <tr key={login.uuid}>
+                <td data-th="Image" className="align-middle">
+                  <img
+                    src={picture.medium}
+                    alt={"profile image for " + name.first + " " + name.last}
+                    className="img-responsive"
+                  />
+                </td>
+                
+              </tr>
+            );
+          })
+        ) : (
+          <></>
+        )}
+      </tbody>
+    );
+  }
+  
+  export default DataBody;
