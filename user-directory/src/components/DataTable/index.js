ab4e0b7c9e549963fa58b9595bd1c30a44e1e303
hilow2341 <60238749+hilow2341@users.noreply.github.com>
2020-07-17 17:14:57 -0400

    Started data table

---
+++ user-directory/src/components/DataTable/index.js
@@ -0,0 +1,9 @@
+import React, { useContext } from "react";
+import DataBody from "../DataBody";
+import "./DataTable.css";
+import DataAreaContext from "../../utils/DataAreaContext";
+
+const DataTable = () => {
+    const context = useContext(DataAreaContext);
+
+}
